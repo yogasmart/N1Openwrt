43ba1ab3047c837a809966553286b76eaf527599
Bosheng.Chen <Bosheng.Chen@infosys.com>
2020-11-16 15:50:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+![N1](https://github.com/mingxiaoyu/N1Openwrt/workflows/N1/badge.svg)
+
+--------------------
+https://docs.github.com/en/actions/reference/virtual-environments-for-github-hosted-runners#supported-runners-and-hardware-resources
+
+--------------------
+自用 N1 Openwrt
+
+User: root
+Password: Password
+Default IP: 192.168.32.2
+
+defalut firewall: iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+
+app list:
+ adbyby
+ ssr+
+ unblockmusic
+ ddns
+ samba4
+ KMS 
+ frp
+ docker
+ cifs(挂载 SMB/CIFS 网络共享文件夹)
+ vsftpd
+ usb_printer
+ ipsec-server
+ flowoffload
+ 
+ 
+ 
---
+++ diy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+echo '修改机器名称'
+sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+# firewall custom
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+cat > helloworld/luci-app-ssr-plus/root/etc/ssr/black.list << EOF
+services.googleapis.cn
+googleapis.cn
+popd
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+#echo '网易云音乐'
+#git clone  --depth=1 https://github.com/project-openwrt/luci-app-unblockneteasemusic.git 
+
+# Add luci-app-jd-dailybonus
+git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
+
+# Add ServerChan
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
+popd
+
+# Mod zzz-default-settings
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" package/lean/default-settings/files/zzz-default-settings
